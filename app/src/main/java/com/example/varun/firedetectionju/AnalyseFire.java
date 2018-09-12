5bd5b3765c04b0f579bbb201904fa443867a6bfe
VARUN <ivarun22@gmail.com>
2018-09-13 02:21:19 +0530

    opencv imported now

--- app/src/main/java/com/example/varun/firedetectionju/AnalyseFire.java
+++ app/src/main/java/com/example/varun/firedetectionju/AnalyseFire.java
@@ -1,23 +1,167 @@
 package com.example.varun.firedetectionju;
 
-import java.io.File;
+import android.util.Log;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AnalyseFire {
 
-    String pathname = "/storage/emulated/0/Android/data/com.example.varun.firedetectionju/files/FIRE_SAMPLE.jpg";
+    public int fireCheck(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
+        Mat imgHSV = inputFrame.rgba();
+        Imgproc.cvtColor(imgHSV, imgHSV, Imgproc.COLOR_RGBA2RGB);
+        int x1 = imgHSV.rows();
+        int y1 = imgHSV.cols();
+        Imgproc.cvtColor(imgHSV, imgHSV, Imgproc.COLOR_RGB2HSV);
+        List<Mat> lRgb = new ArrayList<Mat>(3);
+        Core.split(imgHSV, lRgb);
+        Mat hChannel = lRgb.get(0);
+        Mat sChannel = lRgb.get(1);
+        Mat vChannel = lRgb.get(2);
+        Core.multiply(sChannel, new Scalar(2), sChannel);
+        Mat newS = sChannel.clone();
+        Scalar meanV = Core.mean(vChannel);
+        Core.subtract(vChannel, meanV, vChannel);
+        Core.multiply(vChannel, new Scalar(1.5), vChannel);
+        Core.add(vChannel, meanV, vChannel);
+        Mat newV = vChannel.clone();
+        List<Mat> listMat = Arrays.asList(hChannel, sChannel, vChannel);
+        Core.merge(listMat, imgHSV);
+        Imgproc.cvtColor(imgHSV, imgHSV, Imgproc.COLOR_HSV2RGB);
 
-    public int fireCheck(){
-        File imgFile = new  File(pathname);
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        //Here complex conjugate transpose was used in MATLAB.
+        Mat h_col_sum = sum(hChannel);
+        h_col_sum = sum(h_col_sum.t());
+        Mat s_col_sum = sum(newS);
+        s_col_sum = sum(s_col_sum.t());
+        Mat v_col_sum = sum(newV);
+        v_col_sum = sum(v_col_sum.t());
+
+        Scalar Y = new Scalar(h_col_sum.get(0,0)[0]/(x1*y1));
+        Scalar C = new Scalar(s_col_sum.get(0,0)[0]/(x1*y1));
+        Scalar B = new Scalar(v_col_sum.get(0,0)[0]/(x1*y1));
+
+        //Double precision not done
+        List<Mat> klm = new ArrayList<Mat>(3);
+        Core.split(imgHSV, klm);
+        Mat k = klm.get(0);
+        Mat l = klm.get(1);
+        Mat m = klm.get(2);
+        Log.d("MyTAG", "test");
+
+        //Here complex conjugate transpose was used in MATLAB.
+        Mat L1 = sum(k);
+        L1 = sum(L1.t());
+        Mat a1 = sum(l);
+        a1 = sum(a1.t());
+        Mat b1 = sum(m);
+        b1 = sum(b1.t());
+
+        Scalar RED = new Scalar(L1.get(0,0)[0]/(x1*y1));
+        Scalar GREEN = new Scalar(a1.get(0,0)[0]/(x1*y1));
+        Scalar BLUE = new Scalar(b1.get(0,0)[0]/(x1*y1));
+
+        Mat allBlack = Mat.zeros(imgHSV.rows(), imgHSV.cols(), CvType.CV_8UC1);
+        List<Mat> temp_r = Arrays.asList(k, allBlack, allBlack);
+        List<Mat> temp_g = Arrays.asList(allBlack, l, allBlack);
+        List<Mat> temp_b = Arrays.asList(allBlack, allBlack, m);
+        Mat red = new Mat();
+        Mat green = new Mat();
+        Mat blue = new Mat();
+        Core.merge(temp_r, red);
+        Core.merge(temp_g, green);
+        Core.merge(temp_b, blue);
+
+        Log.d("MyTAG", "test2");
+
+        Mat d = Mat.zeros(x1, y1, CvType.CV_8UC1);
+        a1 = Mat.zeros(x1, y1, CvType.CV_8UC1);
+        for(int row=1; row<x1; row++){
+            for(int col=1; col<y1; col++) {
+                if(k.get(row, col)[0] >= l.get(row, col)[0])
+                    a1.put(row, col, 1);
+                else
+                    a1.put(row, col, 0);
+            }
         }
-        if(imgFile.exists()){
-            return 1;
+        Log.d("MyTAG", "test3");
+
+        b1 = Mat.zeros(x1, y1, CvType.CV_8UC1);
+        for(int row=1; row<x1; row++){
+            for(int col=1; col<y1; col++) {
+                if((k.get(row, col)[0] >= RED.val[0]) && (l.get(row, col)[0] >= GREEN.val[0]) && (m.get(row, col)[0] >= BLUE.val[0]))
+                    b1.put(row, col, 1);
+                else
+                    b1.put(row, col, 0);
+            }
         }
-        else{
-            return 0;
+        Log.d("MyTAG", "test4");
+
+        Mat g = Mat.zeros(x1, y1, CvType.CV_8UC1);
+        for(int row=1; row<x1; row++){
+            for(int col=1; col<y1; col++) {
+                if((newS.get(row, col)[0] <= 0.38) && (newV.get(row, col)[0] >= 1))
+                    g.put(row, col, 1);
+                else
+                    g.put(row, col, 0);
+            }
         }
+        Log.d("MyTAG", "test5");
+
+        Mat a2 = Mat.zeros(x1, y1, CvType.CV_8UC1);
+        Mat b2 = Mat.zeros(x1, y1, CvType.CV_8UC1);
+        Mat h = Mat.zeros(x1, y1, CvType.CV_8UC1);
+        for(int row=1; row<x1; row++){
+            for(int col=1; col<y1; col++) {
+                if((g.get(row, col)[0]!=0) && (a1.get(row, col)[0]!=0) && (b1.get(row, col)[0]==1)) {
+                    newV.put(row, col, 1);
+                    h.put(row, col, 1);
+                    a2.put(row, col, row);
+                    b2.put(row, col, col);
+                }
+                else {
+                    newV.put(row, col, 0);
+                    h.put(row, col, 0);
+                }
+            }
+        }
+        Log.d("MyTAG", "test6");
+
+        Mat im_th = new Mat();
+        Imgproc.threshold(newV, im_th, 220 , 255 , Imgproc.THRESH_BINARY_INV);
+        Mat im_floodfill = im_th.clone();
+        Point flood=new Point(0,0);
+        Mat mask = Mat.zeros(new Size(imgHSV.width() + 2, imgHSV.height() + 2), CvType.CV_8UC1);
+        Imgproc.floodFill(im_floodfill, mask, flood, Scalar.all(255));
+        Mat im_floodfill_inv = new Mat();
+        Core.bitwise_not(im_floodfill, im_floodfill_inv);
+        Mat im_out = new Mat();
+        List<Mat> imfill = Arrays.asList(im_th, im_floodfill_inv);
+        Core.merge(imfill, im_out);
+
+        Log.d("MyTAG", "test7");
+        return 0;
+        //return imgHSV;
+    }
+
+    private Mat sum (Mat matArray) {
+        double sum=0.0;
+        Mat ret_mat = new Mat(1, matArray.cols(), CvType.CV_8UC1);
+        for(int col=0; col<matArray.cols(); col++){
+            for(int row=0; row<matArray.rows(); row++) {
+                sum += matArray.get(row, col)[0];
+            }
+            ret_mat.put(0, col, sum);
+            sum=0;
+        }
+        return ret_mat;
     }
 }
--- app/src/main/java/com/example/varun/firedetectionju/MainActivity.java
+++ app/src/main/java/com/example/varun/firedetectionju/MainActivity.java
@@ -1,413 +1,124 @@
+
 package com.example.varun.firedetectionju;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.ImageFormat;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CameraMetadata;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.TotalCaptureResult;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.media.Image;
-import android.media.ImageReader;
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Size;
-import android.util.SparseIntArray;
-import android.view.Surface;
-import android.view.TextureView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.widget.Toast;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextureView textureView;
-    private TextView output;
-    Button mainButton;
-    ImageButton cancelButton;
-    ProgressBar progressBar;
-
-    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static{
-        ORIENTATIONS.append(Surface.ROTATION_0,90);
-        ORIENTATIONS.append(Surface.ROTATION_90,0);
-        ORIENTATIONS.append(Surface.ROTATION_180,270);
-        ORIENTATIONS.append(Surface.ROTATION_270,180);
-    }
-
-    private String cameraId;
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraCaptureSessions;
-    private CaptureRequest.Builder captureRequestBuilder;
-    private Size imageDimension;
-
-    private File file;
-    private static final int REQUEST_CAMERA_PERMISSION = 200;
-    private Handler mBackgroundHandler;
-    private HandlerThread mBackgroundThread;
-    private Handler handler;
-
-    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
-        @Override
-        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
-            openCamera();
-        }
-
-        @Override
-        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) { }
-
-        @Override
-        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-            return false;
-        }
-
-        @Override
-        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) { }
-    };
-
-    CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            cameraDevice = camera;
-            createCameraPreview();
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
-            cameraDevice.close();
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice cameraDevice, int i) {
-            cameraDevice.close();
-            cameraDevice=null;
-        }
-    };
-
-    ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
-        @Override
-        public void onImageAvailable(ImageReader imageReader) {
-            mBackgroundHandler.post(new ImageSaver(imageReader.acquireLatestImage(), file));
-        }
-    };
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textureView = findViewById(R.id.textureView);
-        assert textureView != null;
-        textureView.setSurfaceTextureListener(textureListener);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CAMERA},
+                1);
 
-        mainButton = findViewById(R.id.mainButton);
-        cancelButton = findViewById(R.id.cancelButton);
-        progressBar = findViewById(R.id.progressBar);
-        output = findViewById(R.id.output);
-        progressBar.setVisibility(View.INVISIBLE);
-        cancelButton.setVisibility(View.INVISIBLE);
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraview);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
 
-        handler = new Handler(getApplicationContext().getMainLooper());
-        mainButton.setOnClickListener(new View.OnClickListener() {
+        baseLoaderCallback = new BaseLoaderCallback(this) {
             @Override
-            public void onClick(View view) {
-                mainButton.setEnabled(false);
-                takePicture();
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case LoaderCallbackInterface.SUCCESS:
+                        Log.d("MyTAG", "Loader Interface Success.");
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
             }
-        });
+        };
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        startBackgroundThread();
-        if(textureView.isAvailable())
-            openCamera();
-        else
-            textureView.setSurfaceTextureListener(textureListener);
-    }
-
-    @Override
-    protected void onPause() {
-        stopBackgroundThread();
-        super.onPause();
-    }
-
-    private void openCamera() {
-        CameraManager manager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
-        try{
-            cameraId = manager.getCameraIdList()[0];
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
-            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-            assert map != null;
-            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
-            //Check realtime permission if run higher API 23
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-            {
-                ActivityCompat.requestPermissions(this,new String[]{
-                        Manifest.permission.CAMERA,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                },REQUEST_CAMERA_PERMISSION);
-                return;
-            }
-            manager.openCamera(cameraId,stateCallback,null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void createCameraPreview() {
-        try{
-            SurfaceTexture texture = textureView.getSurfaceTexture();
-            assert  texture != null;
-            texture.setDefaultBufferSize(imageDimension.getWidth(),imageDimension.getHeight());
-            Surface surface = new Surface(texture);
-            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            captureRequestBuilder.addTarget(surface);
-            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    if(cameraDevice == null)
-                        return;
-                    cameraCaptureSessions = cameraCaptureSession;
-                    updatePreview();
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    Toast.makeText(MainActivity.this, "Changed", Toast.LENGTH_SHORT).show();
-                }
-            },null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void updatePreview() {
-        if(cameraDevice == null)
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE,CaptureRequest.CONTROL_MODE_AUTO);
-        try{
-            cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(),null,mBackgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "OpenCV Load Failed!", Toast.LENGTH_SHORT).show();
+        } else {
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == REQUEST_CAMERA_PERMISSION)
-        {
-            if(grantResults[0] != PackageManager.PERMISSION_GRANTED)
-            {
-                Toast.makeText(this, "You can't use camera without permission", Toast.LENGTH_SHORT).show();
-                finish();
-            }
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
         }
     }
 
-    private void takePicture() {
-        if(cameraDevice == null)
-            return;
-        CameraManager manager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
-        try{
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
-            Size[] jpegSizes = null;
-            if(characteristics != null)
-                jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-                        .getOutputSizes(ImageFormat.JPEG);
-
-            //Capture image with custom size
-            int width = 640;
-            int height = 480;
-            if(jpegSizes != null && jpegSizes.length > 0)
-            {
-                width = jpegSizes[0].getWidth();
-                height = jpegSizes[0].getHeight();
-            }
-            final ImageReader reader = ImageReader.newInstance(width,height,ImageFormat.JPEG,1);
-            List<Surface> outputSurface = new ArrayList<>(2);
-            outputSurface.add(reader.getSurface());
-            outputSurface.add(new Surface(textureView.getSurfaceTexture()));
-
-            final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-            captureBuilder.addTarget(reader.getSurface());
-            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-
-            //Check orientation base on device
-            int rotation = getWindowManager().getDefaultDisplay().getRotation();
-            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION,ORIENTATIONS.get(rotation));
-
-            file = new File(getExternalFilesDir(null), "FIRE_SAMPLE.jpg");
-
-            reader.setOnImageAvailableListener(readerListener,mBackgroundHandler);
-            final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
-                @Override
-                public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
-                    super.onCaptureCompleted(session, request, result);
-                    Toast.makeText(MainActivity.this, "Image Saved", Toast.LENGTH_SHORT).show();
-                    createCameraPreview();
-                    try {
-                        generateOutput();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-
-            cameraDevice.createCaptureSession(outputSurface, new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    try{
-                        cameraCaptureSession.capture(captureBuilder.build(),captureListener,mBackgroundHandler);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                }
-            },mBackgroundHandler);
-
-
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        //imgHSV = new Mat(width, height, CvType.CV_8UC3);
+        //imgThresholded = new Mat(width, height, CvType.CV_8UC3);
     }
 
-    private void stopBackgroundThread() {
-        mBackgroundThread.quitSafely();
-        try{
-            mBackgroundThread.join();
-            mBackgroundThread= null;
-            mBackgroundHandler = null;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void onCameraViewStopped() {
+
     }
 
-    private void startBackgroundThread() {
-        mBackgroundThread = new HandlerThread("Camera Background");
-        mBackgroundThread.start();
-        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
-    }
-
-    private static class ImageSaver implements Runnable {
-
-        private final Image mImage;
-        private final File mFile;
-
-        ImageSaver(Image image, File file) {
-            mImage = image;
-            mFile = file;
-        }
-
-        @Override
-        public void run() {
-            ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
-            byte[] bytes = new byte[buffer.remaining()];
-            buffer.get(bytes);
-            FileOutputStream output = null;
-            try {
-                output = new FileOutputStream(mFile);
-                output.write(bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                mImage.close();
-                if (null != output) {
-                    try {
-                        output.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-
-    private void generateOutput() throws InterruptedException {
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                progressBar.setVisibility(View.VISIBLE);
-                cancelButton.setVisibility(View.VISIBLE);
-                mainButton.setText(R.string.analysis_button_msg);
-            }
-        });
-
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         final MyAsyncTask task = new MyAsyncTask();
-        task.execute();
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                task.cancel(true);
-            }
-        });
+        task.execute(inputFrame);
+        return inputFrame.rgba();
     }
 
-    private class MyAsyncTask extends AsyncTask<Void, Integer, Integer> {
+    private class MyAsyncTask extends AsyncTask<CameraBridgeViewBase.CvCameraViewFrame, Integer, Integer> {
 
         @Override
-        protected Integer doInBackground(Void... voids) {
+        protected Integer doInBackground(CameraBridgeViewBase.CvCameraViewFrame... params) {
             AnalyseFire fire = new AnalyseFire();
-            return fire.fireCheck();
+            return fire.fireCheck(params[0]);
         }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            output.setText(R.string.analysis_msg);
         }
 
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            int fireCheck = integer;
-            if (fireCheck==0)
-                output.setText("No FIRE Detected!");
-            else
-                output.setText("FIRE Detected!");
-
-
-            progressBar.setVisibility(View.INVISIBLE);
-            cancelButton.setVisibility(View.INVISIBLE);
-            mainButton.setText(R.string.main_button);
-            mainButton.setEnabled(true);
         }
 
         @Override
         protected void onCancelled() {
             super.onCancelled();
-            progressBar.setVisibility(View.INVISIBLE);
-            cancelButton.setVisibility(View.INVISIBLE);
-            mainButton.setText(R.string.main_button);
-            mainButton.setEnabled(true);
-            output.setText(R.string.analysis_cancel_msg);
+            if (cameraBridgeViewBase != null) {
+                cameraBridgeViewBase.disableView();
+            }
         }
     }
 }
