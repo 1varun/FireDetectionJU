28db403d1dcce1dc6d01222aeeb67b0d7b2ac933
VARUN <ivarun22@gmail.com>
2018-09-16 06:53:17 +0530

    Copied Ramendra Code. not working

--- app/src/main/java/com/example/varun/firedetectionju/AnalyseFire.java
+++ app/src/main/java/com/example/varun/firedetectionju/AnalyseFire.java
@@ -1,88 +1,85 @@
 package com.example.varun.firedetectionju;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.util.Log;
-
-import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.Math;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnalyseFire {
 
     String pathname = "/storage/emulated/0/Android/data/com.example.varun.firedetectionju/files/FIRE_SAMPLE.jpg";
 
-    public int fireCheck(){
-
-        Mat imgRGB = new Mat(7,7, CvType.CV_8UC1);
-        Log.d("MyTAG", "test1");
-        //Mat imgRGB = new Mat();
-        Mat inputFrame = Imgcodecs.imread(pathname, Imgcodecs.CV_LOAD_IMAGE_COLOR);
-        Log.d("MyTAG", "test2");
+    public void fireCheck() {
+        Mat imgRGB = new Mat();
+        Mat imgYCbCr = new Mat();
+        Mat inputFrame = Imgcodecs.imread(pathname);
         Imgproc.cvtColor(inputFrame, imgRGB, Imgproc.COLOR_RGBA2RGB);
-        Log.d("MyTAG", "test3");
+        Imgproc.cvtColor(imgRGB, imgYCbCr, Imgproc.COLOR_BGR2YCrCb);
         int width = imgRGB.rows();
         int height = imgRGB.cols();
-        Log.d("MyTAG", Integer.toString(width));
-        Log.d("MyTAG", Integer.toString(height));
-        Mat imgYCbCr = new Mat();
-        Imgproc.cvtColor(imgRGB, imgYCbCr, Imgproc.COLOR_RGB2YCrCb);
-        double rMean,gMean,bMean;
-        double rSum = 0.0,gSum = 0.0,bSum = 0.0;
+        Log.d("MyTAG", "Width: "+Integer.toString(width));
+        Log.d("MyTAG", "Height: "+Integer.toString(height));
 
-        double[] rgb;
-        double[] ycbcr;
-        double yMean,cbMean,crMean;
-        double ySum = 0.0,cbSum = 0.0,crSum = 0.0;
-
-        for(int y = 0;y<height;y++) {
-            for(int x = 0;x<width;x++) {
-                rgb = imgRGB.get(x, y);
-                double rValue = rgb[0];
-                double gValue = rgb[1];
-                double bValue = rgb[2];
-                ycbcr =imgYCbCr.get(x, y);
-                double yValue = ycbcr[0];
-                double crValue = ycbcr[1];
-                double cbValue = ycbcr[2];
-                rSum += rValue;
-                gSum += gValue;
-                bSum += bValue;
-                ySum += yValue;
-                cbSum += cbValue;
-                crSum += crValue;
-            }
-        }
-        Log.d("MyTAG", "test4");
+        List<Mat> lRgb = new ArrayList<>(3);
+        Core.split(imgRGB, lRgb);
+        Mat rChannel = lRgb.get(0);
+        Mat gChannel = lRgb.get(1);
+        Mat bChannel = lRgb.get(2);
+        List<Mat> lycbcr = new ArrayList<>(3);
+        Core.split(imgYCbCr, lycbcr);
+        Mat yChannel = lycbcr.get(0);
+        Mat cbChannel = lycbcr.get(1);
+        Mat crChannel = lycbcr.get(2);
 
         int totalPixels = width*height;
 
-        rMean = (double)(rSum)/totalPixels;
-        gMean = (double)(gSum)/totalPixels;
-        bMean = (double)(bSum)/totalPixels;
+        double rMean,gMean,bMean;
+        Scalar bSum = Core.sumElems(rChannel);
+        Scalar gSum = Core.sumElems(gChannel);
+        Scalar rSum = Core.sumElems(bChannel);
+        rMean = (rSum.val[0])/totalPixels;
+        gMean = (gSum.val[0])/totalPixels;
+        bMean = (bSum.val[0])/totalPixels;
 
-        yMean = (double)(ySum)/totalPixels;
-        cbMean = (double)(cbSum)/totalPixels;
-        crMean = (double)(crSum)/totalPixels;
+        double yMean,cbMean,crMean;
+        Scalar ySum = Core.sumElems(yChannel);
+        Scalar cbSum = Core.sumElems(cbChannel);
+        Scalar crSum = Core.sumElems(crChannel);
+        yMean = (ySum.val[0])/totalPixels;
+        cbMean =(cbSum.val[0])/totalPixels;
+        crMean =(crSum.val[0])/totalPixels;
 
+        Log.d("MyTAG", Double.toString(rSum.val[0]));
+        Log.d("MyTAG", Double.toString(gSum.val[0]));
+        Log.d("MyTAG", Double.toString(bSum.val[0]));
+        Log.d("MyTAG", Double.toString(ySum.val[0]));
+        Log.d("MyTAG", Double.toString(cbSum.val[0]));
+        Log.d("MyTAG", Double.toString(crSum.val[0]));
+        Log.d("MyTAG", Double.toString(rMean));
+        Log.d("MyTAG", Double.toString(gMean));
+        Log.d("MyTAG", Double.toString(bMean));
+        Log.d("MyTAG", Double.toString(yMean));
+        Log.d("MyTAG", Double.toString(cbMean));
+        Log.d("MyTAG", Double.toString(crMean));
+
+
+        double[] rgb;
+        double[] ycbcr;
         boolean rgbFlag = false;
         boolean yCbCrFlag = false;
 
+        Log.d("MyTAG", "Entering Loop");
         for(int y = 0;y<height;y++) {
+            //Log.d("MyTAG", Integer.toString(y));
             for(int x = 0;x<width;x++) {
                 rgb = imgRGB.get(x, y);
-                double rValue = rgb[0];
+                double rValue = rgb[2];
                 double gValue = rgb[1];
-                double bValue = rgb[2];
+                double bValue = rgb[0];
                 ycbcr =imgYCbCr.get(x, y);
                 double yValue = ycbcr[0];
                 double crValue = ycbcr[1];
@@ -98,16 +95,16 @@ public class AnalyseFire {
                         && (cbValue <= 120.0 && crValue >= 150.0)) {
                     yCbCrFlag = true;
                 }
+                if(rgbFlag && yCbCrFlag)
+                    break;
             }
         }
-        Log.d("MyTAG", "test5");
+        Log.d("MyTAG", "Loop Over");
         if(rgbFlag && yCbCrFlag) {
             Log.d("MyTAG", "fire");
-            return 1;
         }
         else {
             Log.d("MyTAG", "not fire");
-            return 0;
         }
     }
 }
